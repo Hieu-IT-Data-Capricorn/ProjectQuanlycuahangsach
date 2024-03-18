53e877dccf15972e1b20a69bbc532532b1ea6dd8
DELL <DELL@DESKTOP-S20VSSH>
2024-03-18 15:11:55 +0700

    Hello

---
+++ src/main/java/com/mycompany/project_quan_ly_cua_hang_sach/Project_Quan_ly_cua_hang_sach.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.project_quan_ly_cua_hang_sach;
+
+/**
+ *
+ * @author DELL
+ */
+public class Project_Quan_ly_cua_hang_sach {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
